f47e4a7a0b628a7a6c8e4d50974535ef316c97fb
JairoBm13 <j.bautista.m13@outlook.com>
2017-08-29 13:09:24 -0500

    modificaciones al js, agregado de generador de html. Modificaciones sobre el html, ya no necesita meter experiencias por su cuenta. Css still

--- javascript/main.js
+++ javascript/main.js
@@ -39,7 +39,7 @@ var main = function(){
     });
     $('#nav-experience').on('click',function(){
         $('html,body').animate({
-            scrollTop: $("#experience").offset().top
+            scrollTop: $("#experience").offset().top-75
         });
         $('.active').removeClass( "active", 300, "swing" );
         $(this).addClass('active',300,'swing');  
@@ -86,41 +86,85 @@ var main = function(){
     });
 
     //Manejo de fechas
-    var birth = new Date();
-    birth.setDate(13);
-    birth.setFullYear(1996);
-    birth.setMonth(2);
-    var today = new Date();
-    var age = Math.floor((today-birth) / (365.25 * 24 * 60 * 60 * 1000));
-    $('#edad').text(age+'');
+    function yearCalculator(day, year, month){
+        var birth = new Date();
+        birth.setDate(day);
+        birth.setFullYear(year);
+        birth.setMonth(month);
+        var today = new Date();
+        var age = Math.floor((today-birth) / (365.25 * 24 * 60 * 60 * 1000));
+        return age;
+        
+    };
+    $('#edad').text(yearCalculator(13,1996,2)+'');
+    
 
     //Cargar habilidades
-    var programming = [
-        {
+    //Array de habilidades
+var programming = [
+    {
         'habilidad' : 'java',
         'anio' : 2013, 
-        'mes' : 'enero',
+        'mes' : 1,
         'dia' : 21,
-        'mastery' : 0.80,
-        'continua' : true },
-        {
-        'habilidad' : 'c',
-        'anio' : 2013, 
-        'mes' : 'enero',
+        'mastery' : 0.85 },
+    {
+        'habilidad' : 'c#',
+        'anio' : 2015, 
+        'mes' : 2,
         'dia' : 21,
-        'continua' : true },
-        {
-        'habilidad' : 'java',
-        'anio' : 2013, 
-        'mes' : 'enero',
+        'mastery' : 0.70},
+    {
+        'habilidad' : 'HTML5/JavaScript',
+        'anio' : 2014, 
+        'mes' : 7,
         'dia' : 21,
-        'continua' : true },
-        {
-        'habilidad' : 'java',
-        'anio' : 2013, 
-        'mes' : 'enero',
+        'mastery' : 0.65},
+    {
+        'habilidad' : 'GIT',
+        'anio' : 2014, 
+        'mes' : 3,
         'dia' : 21,
-        'continua' : true }
-    ];
+        'mastery' : 0.90},
+    {
+        'habilidad' : 'Shell',
+        'anio' : 2016, 
+        'mes' : 2,
+        'dia' : 21,
+        'mastery' : 0.60},
+    {
+        'habilidad' : 'Android',
+        'anio' : 2017, 
+        'mes' : 1,
+        'dia' : 21,
+        'mastery' : 0.70},
+];
+
+programming.forEach(function(ability){
+    var container = $('<div class="container">');
+    var row = $('<div class="row">');
+    container.append(row);
+    row.append($('<div class="col-md-1 pull-left"><span class="glyphicon glyphicon-asterisk"></span></div>'));
+    var span = $('<span class="label label-primary">');
+    span.attr('id','exp'+ability.habilidad).text(ability);
+    var p = $('<p class="col-sm-4">').append(ability.habilidad)
+        .innertHTML = ability.habilidad+' '+span+' de experiencia';
+            
+    row.append(p);
+    row.append($('<div class="progress">')
+        .append($('<div class="progress-bar">')
+            .attr('role','progressbar')
+            .attr('aria-valuenow','45')
+            .attr('aria-valuemin','0')
+            .attr('aria-valuemax','100')
+            .attr('style','width: '+ability.mastery*100+'%').text(ability.mastery*100+'%')
+    ));
+    $('#programming').append(container);
+});
+
+
+    
 };
 $(document).ready(main);
+
+
